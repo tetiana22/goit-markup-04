d430ef0ae5ae749ff1b0717ebca7be072d7fc428
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2023-07-05 21:32:10 +0100

    change a

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,15 @@
-<script>
-    function openModal() {
-        document.getElementById("modal").style.display = "block";
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
     }
     
-    function closeModal() {
-        document.getElementById("modal").style.display = "none";
-    }
-</script>
+  })();
